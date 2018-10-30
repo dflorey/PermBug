78b08dba59b39e783e99cbb1f1e2691c41945065
dflorey <daniel.florey@gmail.com>
2018-10-30 17:07:21 +0100

    first commit

---
+++ src/com/floreysoft/permbug/client/PermBug.java
@@ -0,0 +1,10 @@
+package com.floreysoft.permbug.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class PermBug implements EntryPoint {
+	public void onModuleLoad() {
+		RootPanel.get();
+	}
+}
